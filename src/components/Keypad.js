b6841b57a5feaeaaa064db4127060c6e3b39923e
Victor <mwendwav1789@gmail.com>
2024-11-01 16:43:21 +0300

    update src

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,26 @@
-// Code Keypad Component Here
+// components/Keypad.js
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+
+function Keypad() {
+  // Event handler function
+  const handleChange = () => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={handleChange}
+        placeholder="Enter your password"
+      />
+    </div>
+  );
+}
+
+function Keypad() {
+  return <div></div>;
 }
 
 export default Keypad;
